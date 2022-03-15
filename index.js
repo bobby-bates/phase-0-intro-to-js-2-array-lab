c9c7fccd3afa5c1b8c9dda372ce0acef2ed03779
Bobby Bates <robertfbatesjr@gmail.com>
2022-03-14 23:08:42 -0400

    Completed assigment

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Write your solution here!
+const cats = ['Milo', 'Otis', 'Garfield']
+
+function destructivelyAppendCat(name) {
+    cats.push(name)
+    // Also:
+    // cats.splice(cats.length, 0, name)
+}
+function destructivelyPrependCat(name) {
+    cats.unshift(name)
+    // Also:
+    // cats.splice(0, 0, name)
+}
+function destructivelyRemoveLastCat(name) {
+    cats.pop(name)
+    // Also:
+    // cats.splice(-1)
+}
+function destructivelyRemoveFirstCat(name) {
+    cats.shift(name)
+    // Also:
+    // cats.splice(0, 1)
+}
+function appendCat(name) {
+    const moarCatsAfter = [...cats, name]
+    return moarCatsAfter
+}
+function prependCat(name) {
+    const moarCatsBefore = [name, ...cats]
+    return moarCatsBefore
+}
+function removeLastCat(name) {
+    return cats.slice(0, 2)
+}
+function removeFirstCat(name) {
+    return cats.slice(1)
+}
